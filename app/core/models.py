8743d29226a5e0653597316aad8ef9e5bd7baca6
MikolajWasowski <miwasowski@gmail.com>
2023-08-10 20:11:07 +0200

    Fix linting

--- app/core/models.py
+++ app/core/models.py
@@ -74,8 +74,9 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+
 class Ingredient(models.Model):
-    """Ingredients for filtering recipes."""
+    """Ingredient for recipes."""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
